a6aed76dae3ba0ed2880fa024e0fdeea4f26f4e3
APIron-lab <0cy32634231033f@ezweb.ne.jp>
2025-11-28 03:00:09 +0900

    Update Lambda handler / ignore build dir

--- lambda_http/main.py
+++ lambda_http/main.py
@@ -1,11 +1,8 @@
 # lambda_http/main.py
 
-from __future__ import annotations
-
 from mangum import Mangum
-
 from backend.fastapi_app.main import app
 
-# AWS Lambda 用エントリポイント
+# Mangum アダプタを使って FastAPI を Lambda 対応させる
 handler = Mangum(app)
 
